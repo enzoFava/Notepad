60d4a505f2e025b5ce995ab5e5427ff0a063051d
enzoFava <117693440+enzoFava@users.noreply.github.com>
2024-06-26 17:27:55 -0300

    update URLs for deploy

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // "https://resume-creator-server.vercel.app/" || 
+  baseURL: "https://notepad-server.vercel.app/", // "http://localhost:5000" || 
 });
 
 export const getNotes = () => api.get("/");
