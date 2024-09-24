1f4d6d32bcfefc120217dcb1e8d2c1fc36b39336
enzoFava <117693440+enzoFava@users.noreply.github.com>
2024-09-24 18:15:06 -0300

    update db

--- server/server.js
+++ server/server.js
@@ -6,11 +6,7 @@ import cors from "cors";
 const port = process.env.PORT || 5000;
 const app = express();
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
     rejectUnauthorized: false,
